184e25257e51069687082520d6d0f9530d8b0acd
lucasdezarraga-creator <lucasdezarraga@gmail.com>
2026-03-20 15:43:24 -0400

    I've updated the css file. The visuals should be better now.

--- RasberryPiLangUI/app/welcome/welcome.tsx
+++ RasberryPiLangUI/app/welcome/welcome.tsx
@@ -1,10 +1,14 @@
 import waveBottom from "./WaveTileBottom.png"
 import waveTop from "./WaveTileTop.png"
 import waves from "./Waves.png"
+import Scanner from '../Scanner/Scanner';
 
 export function Welcome() {
   return (
     <>
+      <div>
+        <Scanner active = {true} />
+      </div>
       <div 
         className = "fixed bottom-0 left-0 w-full h-[90px]"
         style={{
