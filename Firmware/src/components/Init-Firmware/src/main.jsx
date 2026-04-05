bd50655836f9ac19e15d2e86a995909a8dff4948
Matthew Quintero <matthewquintero1@yahoo.com>
2026-04-04 22:05:36 -0400

    added amy buttons

--- Firmware/src/components/Init-Firmware/src/main.jsx
+++ Firmware/src/components/Init-Firmware/src/main.jsx
@@ -2,15 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Welcome } from "./welcome.tsx"
 import Drop from "./Drop.jsx"
-import { Home } from "./Home.jsx"
+import  Home  from "./Home.jsx"
 import './index.css'              
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div style={{ position: "relative", }}>
+    <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
+      
+      {/* Background layer */}
       <Welcome />
+      
+      {/* Home sits on top of Welcome */}
+      <div style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        zIndex: 5          // above Welcome (z:0) but below Drop/waves (z:10)
+      }}>
+        <Home />
+      </div>
+
+      {/* Drop floats above everything */}
       <Drop />
-      <Home/>
 
     </div>
   </StrictMode>
